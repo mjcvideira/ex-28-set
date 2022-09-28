5640cd6a748dd1c1af1d40bcdfe0fa46b06f4400
mcjvideira <miguelcorreiavideira@enta.pt>
2022-09-28 12:03:02 +0000

    a

---
+++ ex7.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    vendas = [
+          [10, 20 , 30, 40 , 50],
+          [15, 25, 35, 45, 55]
+      ]
+   # print(vendas)
+
+    for venda in vendas:
+        print(venda)
+        for v in venda:
+            print(v)
+    x = 0
+    y = 0
+
+    total = 0
+    for x in range(2):
+        totall = 0
+        for y in range(5):
+            #print(f'x={x} y ={y} = {vendas[x][y]}')
+            total = total + vendas[x][y]
+            totall = totall + vendas[x][y]
+        print(f'totalxxxxxxxxxx {total}')
+    print(f'yyyyyyyyyyy {totall}')
+    for x in range(len(vendas)):
+        for y in range(len(vendas[0])):
+            print(f'x={x} y ={y} = {vendas[x][y]}')
